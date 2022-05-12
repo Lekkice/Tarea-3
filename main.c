953f9ee4a7cc6c6390b1ae5089f233d2850e5279
EldritchBear <fernando.aran.serrano@gmail.com>
2022-05-11 21:32:16 -0400

    Algunas funciones complemento y struct palabra

--- main.c
+++ main.c
@@ -3,6 +3,60 @@
 #include <stdlib.h>
 #include "map.H"
 
+typedef struct{           //struct para guardar las apariciones de cada palabra
+    char palabra[128];
+    unsigned long cont;
+}Palabra;
+
+
+int stringEqual(const void * key1, const void * key2) {
+    const char * A = key1;
+    const char * B = key2;
+    return strcmp(A, B) == 0;
+}
+
+long long stringHash(const void * key) {
+    long long hash = 5381;
+    const char * ptr;
+    for (ptr = key; *ptr != '\0'; ptr++) {
+        hash = ((hash << 5) + hash) + tolower(*ptr); /* hash * 33 + c */
+    }    
+    return hash; 
+}
+
+char* next_word (FILE *fp) {
+    char x[1024];
+    /* assumes no word exceeds length of 1023 */
+    if (fscanf(fp, " %1023s", x) == 1)
+        return strdup(x);
+    else
+        return NULL;
+}
+
+int hayQueEliminar(char c, char* string_chars){
+    for(int i=0 ; i<strlen(string_chars) ; i++){
+        if(string_chars[i]==c) return 1;
+    }
+    return 0;
+}
+
+char* quitar_caracteres(char* string, char* c){
+    int i;
+    int j;
+    for(i=0 ; i < strlen(string) ; i++){
+        if(hayQueEliminar(string[i], c)){
+            for(j=i ; j<strlen(string)-1 ;j++){
+                string[j] = string[j+1];
+        }
+        string[j]='\0';
+        i--;
+    }
+}
+
+    return string;
+}
+
+
 int main()
 {
     
@@ -11,5 +65,4 @@ int main()
 
 
 
-    Carg
 }
